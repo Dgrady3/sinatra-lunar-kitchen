e3cac0d493ced3a4937788b1820feece196f7991
Joe Grady <jogrady1@gmail.com>
2014-09-08 10:04:55 -0400

    initial commit

--- models/ingredient.rb
+++ models/ingredient.rb
@@ -1,2 +1,38 @@
-class Ingredient
+# called on by class Recipe, creates new ingredient
+require 'pg'
+
+Class Ingredient
+  attr_reader :id, :name, :ingredient_id
+
+  def initialize(hash)
+    @id = hash['id']
+    @name = hash['name']
+    @ingredient_id = hash['ingredient_id']
+  end
+
+  def self.db_connection
+    begin
+      connection = PG.connect(dbname: 'recipes')
+
+      yield(connection)
+
+    ensure
+      connection.close
+    end
+  end
+
+  def self.all
+    query = 'SELECT * FROM ingredients'
+
+    results = db_connection do |conn|
+            conn.exec(query)
+    end
+    all_ingredients = []
+
+    results.each do |result|
+      all_ingredients << Ingredient.new(result)
+    end
+    all_ingredients
+  end
 end
+
--- models/recipe.rb
+++ models/recipe.rb
@@ -1,2 +1,74 @@
-class Recipe
+# Creates a new recipe, which holds name, id...
+# and an array of class Ingredient
+Class Recipe
+  attr_reader :id, :name, :description, :instructions, :ingredients
+
+  def initialize(hash)
+    @id = hash['id']
+    @name = hash['name']
+    @description = get_description(hash)
+    @instructions = get_instructions(hash)
+    @ingredients = get_ingredients(@id)
+  end
+
+  def get_description(hash)
+    if hash['description'] == nil
+      "This recipe doesn't have a description."
+    else
+      hash['description']
+    end
+  end
+
+  def get_instructions(hash)
+    if hash['instructions'] == nil
+      "This recipe doesn't have any instructions."
+    else
+      hash['instructions']
+    end
+  end
+
+  def self.all
+    query = 'SELECT * FROM recipes'
+
+    results = db_connection do |conn|
+            conn.exec(query)
+    end
+    all_recipes = []
+
+    results.each do |result|
+      all_recipes << Recipe.new(result)
+    end
+    all_recipes
+  end
+
+  def self.find(id)
+    query = "SELECT * FROM recipes WHERE recipes.id = $1"
+
+    finders = db_connection do |conn|
+            conn.exec_params(query, [id])
+    end
+    recipe = []
+
+    finders.each do |data|
+      recipe << Recipe.new(data)
+    end
+    recipe.first
+  end
+
+  def get_ingredients(recipe_id)
+    query = 'SELECT * FROM ingredients WHERE ingredients.recipe_id = $1'
+    results = db_connection do |conn|
+        conn.exec_params(query, [recipe_id])
+    end
+    some_ingredients_for_a_recipe = []
+
+    results.each do |result|
+      some_ingredients_for_a_recipe << Ingredient.new(result)
+    end
+    some_ingredients_for_a_recipe
+  end
 end
+
+
+
+
